d9d573d1c3bc7e7e506ed8194cd784c9793cf982
ToFJ <jehletom@hotmail.com>
2022-01-13 21:28:51 +0100

    basics for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="nav">
+      <Link to="/leaderboard">
+        <p>Placeholder</p>
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
