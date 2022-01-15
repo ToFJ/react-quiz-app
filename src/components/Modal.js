1abb8731926d2a01400f37185455c62465c530e6
ToFJ <jehletom@hotmail.com>
2022-01-15 17:12:47 +0100

    added score + able to go thorugh all questions

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Loading from "./Loading";
 import { useGlobalContext } from "../context";
 
 const Modal = () => {
   const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [answers, setAnswers] = useState([]);
   const { isLoading, data } = useGlobalContext();
   const { results } = data;
   console.log(results);
@@ -11,7 +13,7 @@ const Modal = () => {
   const randomizeAnswers = array => {
     let currentIndex = array.length,
       randomIndex;
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
       [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
@@ -19,10 +21,22 @@ const Modal = () => {
     return array;
   };
 
+  const checkCorrect = name => {
+    if (name === results[index].correct_answer) {
+      setScore(score + 1);
+      if (index <= 8) {
+        setIndex(index + 1);
+      }
+    }
+    if (index <= 8) {
+      setIndex(index + 1);
+    }
+  };
+  console.log(index);
   if (isLoading) {
     return <Loading />;
   }
-
+  console.log(score);
   const newAnswers = [...results[index].incorrect_answers, results[index].correct_answer];
 
   return (
@@ -33,7 +47,13 @@ const Modal = () => {
         <h4 className="question" dangerouslySetInnerHTML={{ __html: results[index].question }}></h4>
         <div className="answers">
           {randomizeAnswers(newAnswers).map((answer, index) => {
-            return <button key={index} dangerouslySetInnerHTML={{ __html: answer }}></button>;
+            return (
+              <button
+                onClick={() => checkCorrect(answer)}
+                key={index}
+                dangerouslySetInnerHTML={{ __html: answer }}
+              ></button>
+            );
           })}
         </div>
       </div>
@@ -42,3 +62,10 @@ const Modal = () => {
 };
 
 export default Modal;
+
+// {index > 9 && (
+//   <div className="finish-screen">
+//     <p>Congratulations! you got {score}/10 correct</p>
+//     <button>Play Again</button>
+//   </div>
+// )}
